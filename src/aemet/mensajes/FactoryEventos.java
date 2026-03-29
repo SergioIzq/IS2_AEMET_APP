cb6a1da4eb03d1e39a3ee1ac0450a07175e03ea9
Sergio Izquierdo <seizquie@gmail.com>
2026-03-29 16:52:53 +0200

    Organizo proyecto en paquetes junto a clases inciales

--- src/aemet/mensajes/FactoryEventos.java
+++ src/aemet/mensajes/FactoryEventos.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package aemet.mensajes;
 
 /**
  *
  * @author Sergio
  */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Trabajo IS2");
-    }
+public class FactoryEventos {
+    
 }
---
+++ src/aemet/notificacion/EstrategiaNotificacion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aemet.notificacion;
+
+/**
+ *
+ * @author Sergio
+ */
+public interface EstrategiaNotificacion {
+    
+}
